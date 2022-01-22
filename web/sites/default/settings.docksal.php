48998f1f3a98c517864480a95ff06073123b2eba
mattschaff <matthew.schaff@gmail.com>
2022-01-22 15:45:54 -0500

    make local services permanent

--- web/sites/default/settings.docksal.php
+++ web/sites/default/settings.docksal.php
@@ -32,7 +32,7 @@ $settings['file_chmod_file'] = 0666;
 $config['system.performance']['css']['preprocess'] = FALSE;
 $config['system.performance']['js']['preprocess'] = FALSE;
 
-$settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
+$settings['container_yamls'][] = __DIR__ . '/local.services.yml';
 $settings['cache']['bins']['render'] = 'cache.backend.null';
 $settings['cache']['bins']['page'] = 'cache.backend.null';
 $settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
